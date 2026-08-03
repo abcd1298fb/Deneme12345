4c970bde3bfb2de5553980b474139b9ca5e50457
abcd1298fb <fatbokproton@proton.me>
2026-08-03 23:49:11 +0300

    Update stream.js

--- api/stream.js
+++ api/stream.js
@@ -1,10 +1,8 @@
 const http = require('http');
 
 module.exports = (req, res) => {
-  // Hedef IPTV Yayın Adresi
   const targetUrl = 'http://185.243.7.47/live/play/WW5ZMVExWlNhVFJ0YVVsclpTdFlTbTE2U0RReFFXMUlPWHB2ZDB0alRUaHJlRWRSYzFKc01rdzJhejA9/1626874';
 
-  // İstenen özel header'lar
   const options = {
     headers: {
       'User-Agent': 'XP Player',
@@ -14,12 +12,10 @@ module.exports = (req, res) => {
   };
 
   http.get(targetUrl, options, (stream) => {
-    // CORS (Cross-Origin) engellerini kaldırıyoruz
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.setHeader('Content-Type', stream.headers['content-type'] || 'video/mp2t');
+    res.setHeader('Content-Type', 'video/mp2t');
 
-    // Yayın akışını istemciye aktar
     stream.pipe(res);
   }).on('error', (err) => {
     console.error('Yayın çekme hatası:', err.message);
